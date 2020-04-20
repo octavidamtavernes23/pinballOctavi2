d4718df5902af97726cd5f22c63cbf34b689ffbd
Octavio Montagud <octavidamtavernes@gmail.com>
2020-04-20 23:30:31 +0200

    incluits petacos i llançaboles

---
+++ Assets/Scripts/Bumpers.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bumpers : MonoBehaviour
+{
+    public float force = 100000000000000.0f;
+    public float forceRadius = 1.0f;
+
+    void OnCollisionEnter()
+    {
+        foreach (Collider col in Physics.OverlapSphere (transform.position, forceRadius))
+        {
+            if(col.GetComponent<Rigidbody>())
+            {
+                col.GetComponent<Rigidbody>().AddExplosionForce(force,transform.position,forceRadius);
+            }
+        }
+    }
+}
